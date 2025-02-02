fe0e6f881314d37358494ae4b00c72bc4a24feae
manaf12 <115929294+manaf12@users.noreply.github.com>
2025-02-02 23:05:49 +0800

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,33 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
+import Links from "../sidebar/links/Links";
 
 const Navbar = () => {
   
   return (
     <div className="navbar">
+      <div className="mobile-menu">
       <Sidebar/>
+      </div>
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          My Portfolio 
+          <Links/>
         </motion.span>
         <div className="social">
      
-          <a href="https://www.facebook.com/manaf.saoub">
-            <img src="/facebook.png" alt="" />
+          <a href="https://www.facebook.com/manaf.saoub" target="_blank" rel="noopener noreferrer">
+            <img src="/facebook.png" alt="facebook" />
           </a>
-          <a href="https://www.linkedin.com/in/manaf-saoub/">
-            <img src="/linkedin.png"  alt="" />
+          <a href="https://www.linkedin.com/in/manaf-saoub/" target="_blank" rel="noopener noreferrer">
+            <img src="/linkedin.png"  alt="linkedin" />
           </a>
-          <a href="https://github.com/manaf12">
-            <img src="/github.png"  alt="" />
+          <a href="https://github.com/manaf12" target="_blank" rel="noopener noreferrer">
+            <img src="/github.png"  alt="github" />
           </a>
         </div>
       </div>
